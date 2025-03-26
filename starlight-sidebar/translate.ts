4a6b9daf0283d9c6c916ba0a08c178465709fabf
Adam Matthiesen <amatthiesen@outlook.com>
2025-03-25 23:10:42 -0700

    Refactor translation handling in sidebar to support dynamic language imports and improve extensibility

--- starlight-sidebar/translate.ts
+++ starlight-sidebar/translate.ts
@@ -1,18 +1,32 @@
-import en from './en.json';
-import es from './es.json';
+/**
+ * To add a new translation for the sidebar, create a new `<lang>.json` in this directory
+ * and add it to the `Translations` below and it will be added as a supported sidebar lang.
+ */
+import fallback from './en.json';
 
-type TranslationKey = keyof typeof en;
+const Translations = ['en'] as const;
 
-function getKey<T extends Record<string, string>, K extends keyof T | keyof typeof en>(
-	json: T,
-	key: K
-) {
-	return json[key] || en[key as keyof typeof en];
-}
+type TranslationLangs = (typeof Translations)[number];
 
-export function getTranslations(key: TranslationKey) {
-	return {
-		en: getKey(en, key),
-		es: getKey(es, key),
-	};
-}
+const jsonFiles = await Promise.all(
+	Translations.map(async (t) => {
+		return { lang: t, data: (await import(/* @vite-ignore */ `./${t}.json`)).default };
+	})
+);
+
+export const getTranslations = (tKey: string) => {
+	const translations = new Map<TranslationLangs, Record<string, string>>();
+
+	for (const translation of Translations) {
+		translations.set(translation, jsonFiles.find((t) => t.lang === translation)?.data);
+	}
+
+	const toReturnObject: Partial<Record<TranslationLangs, string>> = {};
+
+	for (const [key, value] of translations.entries()) {
+		toReturnObject[key] =
+			(value[tKey] as string | undefined) || (fallback[tKey as keyof typeof fallback] as string);
+	}
+
+	return toReturnObject as Record<TranslationLangs, string>;
+};
