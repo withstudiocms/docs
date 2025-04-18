1693d8a1ac4472af8f710f5bf05511f8f222f35a
Adam Matthiesen <amatthiesen@outlook.com>
2025-04-18 04:01:55 -0700

    remove old link plugin

--- astro.config.mts
+++ astro.config.mts
@@ -76,9 +76,6 @@ export default defineConfig({
 			credits: true,
 			tagline: 'A dedicated CMS for Astro DB. Built from the ground up by the Astro community.',
 			disable404Route: true,
-			markdown: {
-				headingLinks: false,
-			},
 			pagefind: false,
 			components: {
 				SiteTitle: './src/starlightOverrides/SiteTitle.astro',
--- src/plugins/rehypePluginKit.ts
+++ src/plugins/rehypePluginKit.ts
@@ -1,11 +1,11 @@
 import rehypeSlug from 'rehype-slug';
 import type { RehypePlugins } from './rehype.types.ts';
-import rehypeAutolinkHeadings from './rehypeAutolink.ts';
+// import rehypeAutolinkHeadings from './rehypeAutolink.ts';
 import rehypeExternalLinks from './rehypeExternalLinks.ts';
 
 export const rehypePluginKit: RehypePlugins = [
 	rehypeSlug,
-	rehypeAutolinkHeadings,
+	// rehypeAutolinkHeadings,
 	rehypeExternalLinks,
 ];
 
